a903c3c3ff8a0f9cb0e91cc4bee45e841c4747df
gokceturr@gmail.com <gokceturr@gmail.com>
2025-09-10 14:34:44 +0000

    product düzeltme

--- models/staging/raw/stg_raw__product.sql
+++ models/staging/raw/stg_raw__product.sql
@@ -9,7 +9,11 @@ source as (
 renamed as (
 
   select
-    *,                         
+    *,   
+       case                                            
+      when s.products_id is not null then s.products_id
+      else s.pdt_id
+    end as products_id,                    
     safe_cast(purchse_price as float64) as purchase_price 
   from source
 
