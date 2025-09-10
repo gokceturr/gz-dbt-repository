a75958c52b4b380a044c763c3bf4b9d5056fd169
gokceturr@gmail.com <gokceturr@gmail.com>
2025-09-10 14:18:06 +0000

    deneme

--- models/staging/raw/stg_raw__product.sql
+++ models/staging/raw/stg_raw__product.sql
@@ -1,14 +1,18 @@
-{{ config(materialized='view') }}
-
 with
+
 source as (
+
   select * from {{ source('raw','product') }}
+
 ),
+
 renamed as (
+
   select
-    * except(pdt_id, purchse_price),
-    pdt_id as products_id,                         
+    *                         
     safe_cast(purchse_price as float64) as purchase_price 
   from source
+
 )
+
 select * from renamed
