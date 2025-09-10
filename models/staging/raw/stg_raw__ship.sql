a31a4f19f11ae2729b1c9b2dfee747a87f38a1a9
gokceturr@gmail.com <gokceturr@gmail.com>
2025-09-10 11:39:34 +0000

    ship tek sütunda tutuldu

--- models/staging/raw/stg_raw__ship.sql
+++ models/staging/raw/stg_raw__ship.sql
@@ -9,11 +9,20 @@ source as (
 renamed as (
 
     select
-        orders_id,
-        shipping_fee,
-        shipping_fee_1,
-        logcost,
-        ship_cost
+        * except(shipping_fee, shipping_fee_1, ship_cost)
+
+        case
+      when SAFE_CAST(shipping_fee   AS FLOAT64) is not null
+        then SAFE_CAST(shipping_fee   AS FLOAT64)
+      when SAFE_CAST(shipping_fee_1 AS FLOAT64) is not null
+        then SAFE_CAST(shipping_fee_1 AS FLOAT64)
+      else null
+    end as shipping_fee,
+
+    -- ship_cost'u sayıya çevir (sembol/boşluk varsa temizle)
+    SAFE_CAST(REGEXP_REPLACE(CAST(ship_cost AS STRING), r'[^0-9.\-]', '') AS FLOAT64) as ship_cost
+  from source
+       
 
     from source
 
