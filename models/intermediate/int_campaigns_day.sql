f5aa11677469c98ed45833b626f59b68103c682c
gokceturr@gmail.com <gokceturr@gmail.com>
2025-09-11 16:59:14 +0000

    günlük özet

---
+++ models/intermediate/int_campaigns_day.sql
@@ -0,0 +1,9 @@
+
+SELECT
+  date_date,
+  ROUND(SUM(ads_cost), 2) AS ads_cost,
+  SUM(impression)         AS ads_impression,
+  SUM(click)              AS ads_clicks
+FROM {{ ref('int_campaigns') }}
+GROUP BY date_date
+ORDER BY date_date DESC
